38a63e8f6abbf530b3353a3ab360764d213c4980
Gabriel Coelho <gabriel.oliveira2426@gmail.com>
2024-05-07 21:53:29 -0300

    add button to random LED

--- Sequence Led with Button/main.ino
+++ Sequence Led with Button/main.ino
@@ -4,6 +4,7 @@
 */
 const int ledPins[] = {4, 6, 8, 10};
 const int buttonPins[] = {5, 7, 9, 11};
+const int startButtonPin = 12;
 const int numButtons = sizeof(buttonPins) / sizeof(buttonPins[0]);
 
 /*
@@ -15,7 +16,9 @@ void setup() {
     pinMode(ledPins[i], OUTPUT);
     pinMode(buttonPins[i], INPUT);
   }
-
+	
+  pinMode(startButtonPin, INPUT);
+  
   // Sequentially turn on and off LEDs
   sequentialLeds();
 }
@@ -32,6 +35,12 @@ void loop() {
       delay(100);
     }
   }
+  // Check if the select button is pressed
+  if (digitalRead(startButtonPin) == HIGH) {
+    selectRandomLedToBlink();
+  }
+  
+  
 }
 
 /*
@@ -48,28 +57,35 @@ void activateLed(int buttonIndex) {
     Sequentially turn on and off the LEDs
 */
 void sequentialLeds() {
-    for (int j = 0; j < 3; j++) {
-        for (int i = 0; i < numButtons; i++) {
-            digitalWrite(ledPins[i], HIGH); // Turn on current LED
-            delay(150); // Wait 100ms
-            digitalWrite(ledPins[i], LOW); // Turn off current LED
-            delay(150); // Wait 100ms before turning on next LED
-        }
-    }
-
-    // Turn on all LEDs for 3 seconds
+  for (int j = 0; j < 3; j++) {
     for (int i = 0; i < numButtons; i++) {
-        digitalWrite(ledPins[i], HIGH);
+      digitalWrite(ledPins[i], HIGH); // Turn on current LED
+      delay(150); // Wait 100ms
+      digitalWrite(ledPins[i], LOW); // Turn off current LED
+      delay(150); // Wait 100ms before turning on next LED
     }
-    delay(2000); // Wait 2 seconds
-    turnOffAllLeds(); // Turn off all LEDs
+  }
+
+  // Turn on all LEDs for 3 seconds
+  for (int i = 0; i < numButtons; i++) {
+    digitalWrite(ledPins[i], HIGH);
+  }
+  delay(2000); // Wait 2 seconds
+  turnOffAllLeds(); // Turn off all LEDs
 }
 
 /*
     Turn off all LEDs
 */
 void turnOffAllLeds() {
-    for (int i = 0; i < numButtons; i++) {
-        digitalWrite(ledPins[i], LOW);
-    }
+  for (int i = 0; i < numButtons; i++) {
+    digitalWrite(ledPins[i], LOW);
+  }
+}
+
+void selectRandomLedToBlink() {
+  int randomIndex = random(numButtons); // Generate a random index
+  digitalWrite(ledPins[randomIndex], HIGH); // Turn on the randomly selected LED
+  delay(1000); // Wait for 1 second
+  digitalWrite(ledPins[randomIndex], LOW); // Turn off the LED
 }
